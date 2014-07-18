656dfe57704b4d63dbe24c268977b8b8093b4d70
Ben Gamari <bgamari.foss@gmail.com>
2014-07-18 18:24:27 -0400

    Rework lookupPair

--- src/Data/QuadTree/Types.hs
+++ src/Data/QuadTree/Types.hs
@@ -21,7 +21,9 @@ pairTuple :: Iso' (Pair a b) (a, b)
 pairTuple = iso (\(Pair a b) -> (a, b)) (\(a, b) -> Pair a b)
 
 lookupPair :: Eq a => a -> [Pair a b] -> Maybe b
-lookupPair a (Pair x y:rest)
-  | a == x    = Just y
-  | otherwise = lookupPair a rest
-lookupPair _ [] = Nothing
+lookupPair a xs = go xs
+  where
+    go (Pair x y:rest)
+      | a == x    = Just y
+      | otherwise = go rest
+    go [] = Nothing
