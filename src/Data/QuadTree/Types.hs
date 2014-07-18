7b7c0ec56a25882b6323ea244aea28ca8f56b611
Ben Gamari <bgamari.foss@gmail.com>
2014-07-18 18:09:59 -0400

    Write lookupPair

--- src/Data/QuadTree/Types.hs
+++ src/Data/QuadTree/Types.hs
@@ -5,6 +5,7 @@
 module Data.QuadTree.Types
     ( Pair (..)
     , pairTuple
+    , lookupPair
     ) where
 
 import Data.Foldable
@@ -18,3 +19,9 @@ data Pair a b = Pair !a !b
 
 pairTuple :: Iso' (Pair a b) (a, b)
 pairTuple = iso (\(Pair a b) -> (a, b)) (\(a, b) -> Pair a b)
+
+lookupPair :: Eq a => a -> [Pair a b] -> Maybe b
+lookupPair a (Pair x y:rest)
+  | a == x    = Just y
+  | otherwise = lookupPair a rest
+lookupPair _ [] = Nothing
